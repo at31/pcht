1e0f57901f96a0c23989628df7c557be1fcb2174
atcrew <atcrew3@gmail.com>
2017-01-07 03:04:43 +0300

    002
    все кнопки работают, события локально

--- public/js/dev/pm/module/pathList.js
+++ public/js/dev/pm/module/pathList.js
@@ -6,6 +6,7 @@ module.exports = function() {
 	this.elJade = require('../view/listEl.jade');
 	this.nestedListJade = require('../view/sublist.jade');
 	this.pdfJade = require('../view/pdfPathList.jade');
+	this.evntDetailJade = require('../view/editdelEvent.jade');
 	this.ymapCeatePath = {};
 	this.selectedEL = [];
 
@@ -26,7 +27,7 @@ module.exports = function() {
 			"data": data
 		}));
 		self.selectedEL.push(data);
-		$('#' + data.postalCode).on('click', {
+		$('[data-po-id=' + data.postalCode + ']').children('.remove-po-list').on('click', {
 			element: data
 		}, self.removeElement);
 		self.loadEvntData(data);
@@ -48,11 +49,16 @@ module.exports = function() {
 				});
 				_data.evnts = rarr;
 				setTimeout(function() {
-					$('#' + _data.postalCode).parent().append(self.nestedListJade({
+					console.log(rarr);
+					$('[data-po-id=' + _data.postalCode + ']').find('i.fa-spinner').remove();
+					$('[data-po-id=' + _data.postalCode + ']').append(self.nestedListJade({
 						"data": rarr
 					}));
-					$('#' + _data.postalCode).parent().find('i.fa-spinner').remove();
-				}, Math.floor(Math.random() * 10000));
+					//$('.evnt-li').children('.show-evnt-detail').on('click',self.showEvntDetail);
+					//$('.evnt-li').children('.remove-evnt-list').on('click',self.removeEvnt);					
+					//$('[data-po-id='+ _data.postalCode+']').on('click',self.showEvntDetail);
+					$('[data-po-id=' + _data.postalCode + ']').on('click', self.listenEvnt);
+				}, /*Math.floor(Math.random() * 10000)*/ 10);
 			},
 			error: function(err) {
 				//console.log(err);
@@ -61,12 +67,69 @@ module.exports = function() {
 
 
 	};
+	this.listenEvnt = function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+
+		var type = true; //delete
+		if ($(e.target).parent().hasClass('show-evnt-detail'))
+			type = false; //'show-detail';
+
+		var op = $(e.currentTarget).attr('data-po-id');
+		var evntId = $(e.target).parent().parent().attr('data-evnt-id');
+		//console.log($(e.target).parent().hasClass('show-evnt-detail'));
+
+
+		for (var i = self.selectedEL.length; i--;) {
+			if (self.selectedEL[i].postalCode == op) {
+				for (var j = self.selectedEL[i].evnts.length; j--;) {
+					if (self.selectedEL[i].evnts[j]._id == evntId) {
+						if (type) {
+							self.selectedEL[i].evnts.splice(j, 1);
+							$(e.target).parent().parent().remove();
+						} else {
+							var _event = self.selectedEL[i].evnts[j];
+							$('.modal-place').html(self.evntDetailJade());
+							$(".flatpickr").flatpickr({
+								enableTime: true,
+							});
+
+							$('#modal').modal('show');
+
+							//$('#datebegin').val(_event.start.format('YYYY-MM-DD hh:mm'));
+							//$('#dateend').val(_event.end.format('YYYY-MM-DD hh:mm'));
+							$('#event-title').val(_event.title);
+							$('#postcode').val(_event.postalCode);
+							$('#status').val(_event.status);
+							$("#description").val(_event.description);
+							$('#executor').val(_event.executor);
+
+							$('#modal').on('hidden.bs.modal', function(e) {
+								console.log('modal hide');
+								self.destroyModal();
+
+							});
+						}
+					}
+				}
+			}
+		}
+
+	};
+	this.destroyModal = function() {
+
+		//$("#save-event-btn").off('click');
+		$('#close-modal-btn').off('click');
+		$('#close-modal-cross-btn').off('click');
+		//$('#delete-event-btn').off();
+		$('.modal-place').html('');
+	};
 
 	this.removeElement = function(e) {
 		e.stopPropagation();
 		for (var i = self.selectedEL.length; i--;) {
 			if (self.selectedEL[i].postalCode == e.data.element.postalCode) {
-				$(e.currentTarget).parent().off();
+				$(e.currentTarget).off();
 				$(e.currentTarget).parent().remove();
 				//$('[data-evnt-id='+e.data.element.postalCode+']').off();
 				//$('[data-evnt-id='+e.data.element.postalCode+']').remove();
@@ -114,16 +177,17 @@ module.exports = function() {
 				text: 'Лист-список заявок к исполнению (форма №1лс)',
 				style: 'header'
 			}, {
-				ul: ularr, style:'lists'
+				ul: ularr,
+				style: 'lists'
 			}],
 			styles: {
 				header: {
 					fontSize: 16,
 					bold: true
 				},
-				lists:{
-					padding:"15px",
-					fontSize:14
+				lists: {
+					padding: "15px",
+					fontSize: 14
 				}
 			}
 		};
